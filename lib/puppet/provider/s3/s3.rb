84b1bb86adb124ea651efbbd067cdf5a3a4a8db4
malnick <malnick@gmail..com>
2015-02-07 15:51:12 -0800

    provider/s3/s3.rb

--- lib/puppet/provider/s3/s3.rb
+++ lib/puppet/provider/s3/s3.rb
@@ -1,8 +1,8 @@
 require 'rubygems' if Puppet.features.rubygems? 
-require 'aws-sdk' #if Puppet.features.awssdk?
+require 'aws-sdk' if Puppet.features.awssdk?
 
 Puppet::Type.type(:s3).provide(:s3) do
-  confine :feature => :awssdk
+  #confine :feature => :awssdk
 
   desc "Run pupppet on a node"
 
