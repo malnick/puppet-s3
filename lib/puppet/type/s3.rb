8e22262a62030a07965fa0a05acdf2ce549b2d4c
malnick <malnick@gmail..com>
2015-02-08 08:46:12 -0800

    type/s3.rb

--- lib/puppet/type/s3.rb
+++ lib/puppet/type/s3.rb
@@ -1,27 +1,27 @@
 Puppet::Type.newtype(:s3) do
-  #@@doc =  %q{Get files from S3
+  @@doc =  %q{Get files from S3
    
-  #    Example:
+      Example:
         
-  #      s3 {'/path/on/my/filesystem':
-  #          ensure              => present,
-  #          source              => '/bucket/subdir/s3_object',
-  #          region              => 'us-east-1', # better for speed if you set it in s3
-  #          access_key_id       => 'ITSASECRET',
-  #          secret_access_key   => 'ITSASECRETTOO',
-  #      }
-  #}
+        s3 {'/path/on/my/filesystem':
+            ensure              => present,
+            source              => '/bucket/subdir/s3_object',
+            region              => 'us-east-1', # better for speed if you set it in s3
+            access_key_id       => 'ITSASECRET',
+            secret_access_key   => 'ITSASECRETTOO',
+        }
+  }
   
   ensurable
 
   newparam(:path, :namevar => true) do
     desc "Path to the file on the local filesystem"
-    #validate do |v|
-    #    path = Pathname.new(v)
-    #    unless path.absolute?
-    #        raise ArgumentError, "Path not absolute: #{path}"
-    #    end
-    #end
+    validate do |v|
+        path = Pathname.new(v)
+        unless path.absolute?
+            raise ArgumentError, "Path not absolute: #{path}"
+        end
+    end
   end
 
   newparam(:source) do
