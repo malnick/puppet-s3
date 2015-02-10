621d9f0362721320561d95c933f0f32fba30874d
malnick <malnick@gmail..com>
2015-02-10 15:21:20 -0800

    init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,12 +2,12 @@ class s3 {
 
     package { 'aws-sdk-core':
         ensure   => present,
-        provider => 'pe_gem',
+        provider => 'gem',
     }
 
     package { 'aws-sdk-resources':
         ensure   => present,
-        provider => 'pe_gem',
+        provider => 'gem',
     }
 
     Package ['aws-sdk-core','aws-sdk-resources'] -> S3 <| |>
