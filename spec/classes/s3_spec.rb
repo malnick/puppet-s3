fe22dca0f701ddf5fe3dae34e2001532f3ad4a7b
Adam Crews <adam.crews@gmail.com>
2015-03-28 00:24:09 -0700

    Make a proper module

---
+++ spec/classes/s3_spec.rb
@@ -0,0 +1,35 @@
+require 'spec_helper'
+
+describe 's3' do
+  context 'supported operating systems' do
+    on_supported_os.each do |os, facts|
+      context "on #{os}" do
+       let(:facts) do
+         facts
+       end
+
+        context "s3 class without any parameters" do
+          let(:params) {{ }}
+          
+          context "is_pe not defined" do
+#            let(:facts) {{ :is_pe => nil }}
+            it { is_expected.to compile.with_all_deps }
+            it { is_expected.to contain_package('aws-sdk').with(
+              :ensure   => 'present',
+              :provider => 'gem',
+            )}
+          end
+
+          context "is_pe true" do
+            let(:facts) {{ :is_pe => true }}
+            it { is_expected.to compile.with_all_deps }
+            it { is_expected.to contain_package('aws-sdk').with(
+              :ensure   => 'present',
+              :provider => 'pe_gem',
+            )}
+          end
+        end
+      end
+    end
+  end
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,7 @@
+require 'puppetlabs_spec_helper/module_spec_helper'
+require 'rspec-puppet-facts'
+include RspecPuppetFacts
+
+#require 'pathname'
+#dir = Pathname.new(__FILE__).parent
+#$LOAD_PATH.unshift(dir, dir + 'lib', dir + '../lib')
